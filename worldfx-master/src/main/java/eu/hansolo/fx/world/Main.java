085903b48a6b72f010de712284a23bc31f5f0ddf
Kasper Kadestål <kadestalkasper@gmail.com>
2022-03-28 20:26:14 +0200

    Update

--- worldfx-master/src/main/java/eu/hansolo/fx/world/Main.java
+++ worldfx-master/src/main/java/eu/hansolo/fx/world/Main.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (c) 2016 by Gerrit Grunwald
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package eu.hansolo.fx.world;
 
@@ -39,12 +25,6 @@ import static eu.hansolo.fx.world.Country.*;
 import static eu.hansolo.fx.world.Country.GB;
 import static eu.hansolo.fx.world.Country.PL;
 
-
-/**
- * User: hansolo
- * Date: 20.09.16
- * Time: 13:37
- */
 public class Main extends Application {
     private static final Random        RND = new Random();
     private              World         world;
